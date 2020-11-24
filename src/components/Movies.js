cf49ae5c1a72b38157969e49d1002a12358d8be8
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-11-24 13:40:18 -0500

    commit tree 3347801cb04c13181db1c64603fcd84c608f9df8

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+			<h1>Movies Page</h1>
+			{movies.map(movie => {
+				return (
+					<div key={movie.title}>
+						<h2>Name: {movie.title}</h2>
+						<h3>Time: {movie.time}</h3>
+						<h3>Genres:</h3>
+						<ul>
+							{movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+						</ul>
+					</div>
+				)})}
     </div>
   );
 };
